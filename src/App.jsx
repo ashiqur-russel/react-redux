80005ba927620be06dfa455134d9bfdac282e968
ashiqurras <ashiqurrussel2@gmail.com>
2023-01-25 19:52:15 +0100

    form design

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,96 @@ import Child from "./components/Child";
 function reducer(state, action) {
   switch (action.type) {
     case "increment":
-      return { count: state.count + 1 };
+      return { count: state.count + action.payload };
     case "decrement":
-      return { count: state.count - 1 };
+      return { count: state.count - action.payload };
     default:
       return state;
   }
 }
 
 function App() {
-  const initialState = { count: 0 };
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <div className="App">
-        <div className="container">
-          <p>{state.count}</p>
-          <div className="btn-group">
-            <button onClick={() => dispatch({ type: "increment" })}>
-              Increment
-            </button>
-            <button onClick={() => dispatch({ type: "decrement" })}>
-              Decrement
-            </button>
+        <form className="form-group" onSubmit={submitHandler}>
+          <div className="form-container">
+            <div className="form-top">
+              <div className="left">
+                <label htmlFor="fname">First Name</label>
+                <input type="text" name="fname" />
+
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" />
+
+                <label htmlFor="education">Education</label>
+
+                <select id="cars" name="cars">
+                  <option value="ssc">SSC</option>
+                  <option value="hsc">HSC</option>
+                  <option value="Bachelor">Bachelor</option>
+                  <option value="Master">Master</option>
+                </select>
+
+                <label htmlFor="feedback">Feedback</label>
+                <textarea type="text" name="feedback" />
+              </div>
+              <div className="right">
+                <label htmlFor="fname">Last Name</label>
+
+                <input type="text" name="lname" />
+
+                <div className="radio-btn">
+                  <p>Gender:</p> {" "}
+                  <input type="radio" id="male" name="gender" value="male" /> {" "}
+                  <label for="html">Male</label> {" "}
+                  <input
+                    type="radio"
+                    id="female"
+                    name="gender"
+                    value="female"
+                  />
+                    <label for="css">Female</label> {" "}
+                  <input
+                    type="radio"
+                    id="others"
+                    name="gender"
+                    value="Others"
+                  />
+                    <label for="others">Others</label>
+                </div>
+                <label htmlFor="pc">Number of Pc</label>
+
+                <div className="numOfPc">
+                  <button className="btnNumb">+</button>
+                  <span name="number" id="number">
+                    0
+                  </span>{" "}
+                  <button className="btnNumb">-</button>
+                </div>
+              </div>
+            </div>
+            <div className="form-bottom">
+              <div>
+                <input
+                  type="checkbox"
+                  id="subscribeNews"
+                  name="subscribe"
+                  value="newsletter"
+                />
+                <label for="subscribeNews">Subscribe to newsletter?</label>
+              </div>
+              <div className="btn-submit1">
+                <button className="btn-submit" type="submit">
+                  Subscribe
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <Child state={state} />
+        </form>
       </div>
     </>
   );
