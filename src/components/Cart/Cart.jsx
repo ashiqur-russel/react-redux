0bfaf2467659ef7867387f0c37eb05f4d55f2361
ashiqurras <ashiqurrussel2@gmail.com>
2023-01-30 17:52:59 +0100

    cart item loaded from redux store

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
-  const cart = [];
+  const { cart } = useSelector((state) => state);
+
+  console.log(cart);
+  console.log();
   return (
     <div className="shopping-cart">
       <div className="title">Shopping Bag</div>
